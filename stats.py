a7552d951e4798db1123c74ac7b81c47333d8184
dod0lp <dodotv007@gmail.com>
2024-08-14 12:15:45 +0200

    Better structuered code

--- stats.py
+++ stats.py
@@ -16,7 +16,7 @@ salary_mean = data[str_salary].mean()
 salary_median = data[str_salary].median()
 salary_variance = data[str_salary].var()
 salary_std_dev = data[str_salary].std()
-salary_quartiles = data[str_salary].quantile([0.25, 0.5, 0.75])
+salary_deciles = data[str_salary].quantile([i/10 for i in range(1, 10)])
 
 # statistics for Years of Experience
 experience_mean = data[str_yoe].mean()
@@ -26,17 +26,24 @@ experience_std_dev = data[str_yoe].std()
 # I will actually be doing decils not quartiles
 experience_deciles = data[str_yoe].quantile([i/10 for i in range(1, 10)])
 
-# simply print the results
-print("Salary Statistics:")
-print(f"Mean: {salary_mean}")
-print(f"Median: {salary_median}")
-print(f"Variance: {salary_variance}")
-print(f"Standard Deviation: {salary_std_dev}")
-print(f"Quartiles:\n{salary_quartiles}\n")
+salary_stats = {
+    "Mean": salary_mean,
+    "Median": salary_median,
+    "Variance": salary_variance,
+    "Standard Deviation": salary_std_dev,
+    "Quartiles": salary_deciles
+}
 
-print("Years of Experience Statistics:")
-print(f"Mean: {experience_mean}")
-print(f"Median: {experience_median}")
-print(f"Variance: {experience_variance}")
-print(f"Standard Deviation: {experience_std_dev}")
-print(f"Quartiles:\n{experience_deciles}")
+experience_stats = {
+    "Mean": experience_mean,
+    "Median": experience_median,
+    "Variance": experience_variance,
+    "Standard Deviation": experience_std_dev,
+    "Quartiles": experience_deciles
+}
+
+for stat_name, stat_value in salary_stats.items():
+    print(f"{stat_name}: {stat_value}")
+
+for experience_name, experience_value in experience_stats.items():
+    print(f"{experience_name}: {experience_value}")
