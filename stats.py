323eba89270cbc6953806af996f8dd8de3dd6c39
dod0lp <dodotv007@gmail.com>
2024-08-19 22:19:30 +0200

    Added separators to the printed text

--- stats.py
+++ stats.py
@@ -9,8 +9,8 @@ from scipy import stats
 
 separator_ = "=" * 100
 
-print(separator_)
-
+def separate() -> None:
+    print(separator_)
 
 # Load the CSV file
 file_path = "Salary Data.csv"
@@ -27,6 +27,8 @@ str_title = "Job Title"
 """
 Print number of unique values in each column
 """
+separate()
+
 def count_unique_values_from_csv(file_path: str, column_name: str, is_int: bool = False) -> str:
     data = pd.read_csv(file_path)
     
@@ -48,22 +50,25 @@ def count_unique_values_from_csv(file_path: str, column_name: str, is_int: bool 
 UniqueValuesCount = True
 if (UniqueValuesCount):
     print(count_unique_values_from_csv(file_path, str_education))
-    print()
+    separate()
     print(count_unique_values_from_csv(file_path, str_salary, is_int=True))
-    print()
+    separate()
     print(count_unique_values_from_csv(file_path, str_yoe, is_int=True))
-    print()
+    separate()
     print(count_unique_values_from_csv(file_path, str_age, is_int=True))
-    print()
+    separate()
     print(count_unique_values_from_csv(file_path, str_gender))
-    print()
+    separate()
     print(count_unique_values_from_csv(file_path, str_title))
-    print()
+    separate()
+
 
 """
 This part is for first part of the project,
 `calculating average, median, variance standard deviation, quartiles for Salary and Years of Experience`
 """
+separate()
+
 def summarize_column_stats(data: pd.DataFrame, column_name: str) -> str:
     mean_value = data[column_name].mean()
     median_value = data[column_name].median()
@@ -85,11 +90,19 @@ def summarize_column_stats(data: pd.DataFrame, column_name: str) -> str:
 
 # simply calculated statistics for Salary using function
 print("Statistics for Salary")
+separate()
 print(summarize_column_stats(data, str_salary))
+separate()
+
+print()
 
 # statistics for Years of Experience
 print("Statistics for Years of Experience")
+separate()
 print(summarize_column_stats(data, str_yoe))
+separate()
+
+print()
 
 # by Gender
 # print(summarize_column_stats(data[data[str_gender] == "Male"], str_salary))
@@ -97,10 +110,10 @@ print(summarize_column_stats(data, str_yoe))
 
 """
 Linear Regression and Correlation of Salary and Years of Experience
-"""
-"""
 Correlation calculations with information about what it means
 """
+separate()
+
 correlation_salary_experience = data[[str_salary, str_yoe]].corr().loc[str_salary, str_yoe]
 
 str_correlation_info_range ="""
@@ -119,7 +132,7 @@ Correlation can range [-1, 1]
 0 to 0.3: Weak positive correlation.
 -0.3 to 0: Weak negative correlation.
 """
-# print(str_correlation_info_range)
+print(str_correlation_info_range)
 
 correlation = correlation_salary_experience.round(3)
 abs_corr = abs(correlation)
@@ -136,13 +149,17 @@ if (correlation > 0):
 elif (correlation < 0):
     correlation_direction = "Negative"
 
+separate()
 print(f"Correlation of Salary and Years of Experience\
 \n Value: {correlation}\n Strength: {correlation_strenght}\n Direction: {correlation_direction}")
+separate()
 
 
 """
 Linear regression model with showing a graph
 """
+separate()
+
 data_drop_nan = data.dropna()
 X = data_drop_nan[[str_yoe]]
 y = data_drop_nan[str_salary]
@@ -162,10 +179,14 @@ plt.legend()
 
 plt.show()
 
+separate()
+
 
 """
 T-Test for Salaries by Gender with information about what its results mean
 """
+separate()
+
 alpha = 0.05  # Significance level; 0.05 is used something like by "default"
 
 male_salaries = data[data[str_gender] == "Male"][str_salary]
@@ -176,6 +197,8 @@ t_stat, p_value = stats.ttest_ind(male_salaries, female_salaries, equal_var=Fals
 print("\nT-Test for Salaries by Gender")
 print(f" T-Statistic: {t_stat.round(2)}")
 print(f" P-Value: {p_value.round(2)}")
+
+print()
 
 if p_value < alpha:
     print("The result is statistically significant so we reject the null hypothesis")
